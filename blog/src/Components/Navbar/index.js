1093e9a0cfba0c07beccbdcf4a935f7178d925d0
jassimran <jvirdi4@gmail.com>
2019-11-27 09:58:37 +0530

    navbar

--- blog/src/Components/Navbar/index.js
+++ blog/src/Components/Navbar/index.js
@@ -7,19 +7,26 @@ import {
     Link,
     useRouteMatch
   } from 'react-router-dom';
-  
-
 class Navbar extends Component {
     render() {
         return (
             <Router>
             <div className="row">
                 <div className="col">
-                    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                    <nav className="navbar navbar-expand-lg navbar-light bg-light">
                         <div className="navbar-brand">
                             <Link to='/'>Home</Link>
                         </div>
-                        
+                        <div className="navbar-nav">
+                            <Link to='/SignIn'>Sign In</Link>
+                        </div>
+                        <div className="navbar-nav">
+                            <Link to='/SignUp'>Sign Up</Link>
+                        </div>
+
+                        <div className="navbar-text ml-5 pl-5">
+                            <i>VbLog</i>
+                        </div>
                     </nav>
                 </div>
             </div >
