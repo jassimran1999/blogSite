ca9b19e46806a857fd463e7cb8a5ccd363c3390d
Vanshika1304 <vanshi1304@gmail.com>
2019-11-27 13:57:50 +0530

    hey

--- blog/src/Components/Navbar/index.js
+++ blog/src/Components/Navbar/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import Signup from './../SignIn';
-import Signin from './../SignUp';
+import Signup from './../SignUp';
+import Signin from './../SignIn';
+//import App from './../../App';
 import 'bootstrap/dist/css/bootstrap.css';
 import {
     BrowserRouter as Router,
@@ -13,25 +14,28 @@ class Navbar extends Component {
     render() {
         return (
             <Router>
+           
             <div className="row">
                 <div className="col">
                     <nav className="navbar navbar-expand-lg navbar-light bg-light">
                         <div className="navbar-brand">
-                            <Link to="/">Home</Link>
+                            <Link to="/home">Home</Link>
                         </div>
-                        <div className="navbar-nav">
+                        <div className="justify-content-end">
                             <Link to="/Signin">Sign In</Link>
                         </div>
-                        <div className="navbar-nav">
+                        <div className="nav navbar-nav pull-xs-right">
                             <Link to="/Signup">Sign Up</Link>
                         </div>
 
-                        <div className="navbar-text ml-5 pl-5">
-                            <i>VbLog</i>
-                        </div>
                     </nav>
                 </div>
             </div >
+            
+            {/* <Route path="/">
+            <App />
+      </Route> */}
+
             <Route path="/Signup">
             <Signup />
       </Route>
