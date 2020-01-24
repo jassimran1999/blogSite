66b73976eb5eb616645c95512d130a24859808b3
jassimran1999 <jvirdi4@gmail.com>
2020-01-25 02:56:46 +0530

    update

---
+++ Backend/node/models/homeQueries.js
@@ -0,0 +1,22 @@
+
+const mongoose = require('mongoose');
+const PostsModel = require('../models/posts');
+const UserModel = require('../models/users');
+
+findHomePosts = function () {
+    mongoose.connection.collection(PostsModel).aggregate([
+        {$lookup:
+                    {
+                        from: UserModel,
+                        localField: 'userId',
+                        foreignField: 'username',
+                        as: 'homedetails'
+                    }
+        }
+    ]).toArray(function (err, res) {
+        if (err)
+            throw err;
+        console.log(JSON.stringify(res));
+    });
+    
+}
