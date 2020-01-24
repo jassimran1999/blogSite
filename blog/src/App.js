1b0c37db54b0a81fd6003c362e24dd191b8f2048
jassimran1999 <jvirdi4@gmail.com>
2020-01-24 22:24:54 +0530

    updated workflow

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,67 +1,15 @@
+import { BrowserRouter } from 'react-router-dom';
 import React,{Component} from 'react';
-import './App.css';
-import Signup from './Components/SignUp';
-import Signin from './Components/SignIn';
-import Home from './Components/Home';
-import Post from './Components/Post';
-import Profile from './Components/Profile';
-import Navbar from './Components/Navbar';
-import AddPost from './Components/AddPost';
-import postData from './Constants/postConstants';
-import userData from './Constants/userConstants';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link,
-  useRouteMatch,
-  Redirect
-} from 'react-router-dom';
+import Appl from './Components/App'
 
 class App extends Component {
   render(){
     return (
-      <div className="outer" >
-      <div className="bg"/>
-      
-      
-      <Router>
-        
-      <Navbar /><br/><br/>
-      <switch>
-       <Route path="/home">
-         <Home />
-           </Route>
-       
-       <Route path="/Signup">
-              <Signup />
-        </Route>
-        <Route path="/Signin">
-              <Signin />
-        </Route>
-  
-        <Route path="/AddPost">
-              <AddPost />
-        </Route>
-        <Route path="/posts/:postId">
-          <div>
-            { postData.map((item) => {return (<Post {...item} /> )})}
-          </div>
-        </Route>
-        <Route path="/users/:userId">
-          <div>
-            { userData.map((item) => {return (<Profile {...item} /> )})}
-          </div>
-        </Route>
-        </switch>
-        </Router>
-  
-        
-        
-        
-      
-  
-     </div>  
+     <div>
+      <BrowserRouter>
+      <Appl />
+      </BrowserRouter>
+     </div>
     );
 
   }
---
+++ blog/src/Components/App/index.js
@@ -0,0 +1,72 @@
+import React,{Component} from 'react';
+import './App.css';
+import Signup from '../SignUp';
+import Signin from '../SignIn';
+import Home from '../Home';
+import Post from '../Post';
+import Profile from '../Profile';
+import Navbar from '../Navbar';
+import AddPost from '../AddPost';
+import postData from '../../Constants/postConstants';
+import userData from '../../Constants/userConstants';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+  Redirect
+} from 'react-router-dom';
+
+export default class extends Component {
+  render(){
+    return (
+      <div className="outer" >
+      <div className="bg"/>
+      
+      
+        
+      <Navbar /><br/><br/>
+      <switch>
+       <Route path="/" exact>
+         <Home />
+           </Route>
+           <Route path="/home" exact>
+         <Redirect to="/"/>
+           </Route>
+       <Route path="/Signup">
+              <Signup />
+        </Route>
+        <Route path="/Signin">
+              <Signin />
+        </Route>
+  
+        <Route path="/AddPost">
+              <AddPost />
+        </Route>
+        <Route path="/posts/:postId">
+          <div>
+            { postData.map((item) => {return (<Post {...item} /> )})}
+          </div>
+        </Route>
+        <Route path="/users/:userId">
+          <div>
+            { userData.map((item) => {return (<Profile {...item} /> )})}
+          </div>
+        </Route>
+        </switch>
+  
+        
+        
+        
+      
+  
+     </div>  
+    );
+
+  }
+  
+  
+}
+
+
