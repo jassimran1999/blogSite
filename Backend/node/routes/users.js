fda1a8afa4295a1cb6075092bdcc6bb69eeac94b
jassimran1999 <jvirdi4@gmail.com>
2020-02-02 01:41:55 +0530

    updated back

--- Backend/node/routes/users.js
+++ Backend/node/routes/users.js
@@ -1,3 +1,4 @@
+'use strict';
 const express = require('express');
 const isAuth = require('../authCheck/authCheck')
 const router = express.Router();
@@ -49,7 +50,7 @@ router.get('/profile/:username', (req, res) => {
         serverStat: '2',
       });
     },
-    ' username name description userPhoto followers following postArr email',
+    ' username name description userPhoto followers following postArr email postsLiked ',
     'postArr',
     'title likes thumbnail views description',
   );
@@ -57,7 +58,6 @@ router.get('/profile/:username', (req, res) => {
 
 
 router.post("/add",upload.single('userPhoto'), (req, res, next) => {
-  console.log(req.file.path);
   UsersModel.addUser(req,res);
 });
 
@@ -66,15 +66,125 @@ router.post("/login",  (req, res, next) => {
   UsersModel.loginUser(req,res);
 });
 
+router.post('/isfollowed/:userMongoId',isAuth , (req,res)=> {
+  UsersModel.findUsers(
+    {_id:req.userData.userId},
+    (response) => {
+        let array = response[0].followingIds;
+        if(array.includes(req.params.userMongoId)) {
+          res.send(
+            {
+              serverStat:0,
+              message:"followed"
+            }
+          )
+        }
+        else {
+          res.send(
+            {
+              serverStat:0,
+              message:"unfollowed"
+            }
+        )};
+    },
+    (error) => {
+      console.log(error);
+      res.status(500).json({
+        serverStat: '2',
+      });
+    },
+    ' followingIds ',)
+  
+})
 
-router.put('/update', isAuth,(req, res) => {
-  UsersModel.updateUsers(req.query, (error, response) => {
-    if (error) console.log('Error is: ', error);
-    if (response) {
-      // console.log("Success response is: ", response);
-      res.send(response);
-    }
-  });
-});
+router.post('/follow/:userMongoId',isAuth , (req,res)=> {
+  UsersModel.findOneAndUpdate(
+    {_id:req.params.userMongoId},
+    { 
+      $inc: { followers: 1 } 
+    }, 
+   {new: true }
+   )
+   .then(()=>
+   UsersModel.findOneAndUpdate(
+    { username: req.userData['username'] }, 
+    { 
+      $inc: { following: 1 } 
+    }, 
+   {new: true }
+   )
+   )
+   .then(()=>{
+    UsersModel.findOneAndUpdate(
+      { username: req.userData['username'] }, 
+      { $push: { followingIds :  req.params.userMongoId} },
+      (err,response) => {
+        if (err) console.log(err);
+          UsersModel.findOneAndUpdate(
+          { _id: req.params.userMongoId }, 
+          { $push: { followedBy : req.userData['userId'] } },
+          (err,response)=>{
+            if (err) console.log(err);
+              res.status(201).json({
+              serverStat:0,
+              message: "followed"
+            });
+          })}
+      )})
+  })
+
+  router.post('/unfollow/:userMongoId',isAuth , (req,res)=> {
+    UsersModel.findOneAndUpdate(
+      {_id:req.params.userMongoId},
+      { 
+        $inc: { followers: -1 } 
+     }, {new: true })
+     .then(()=>
+   UsersModel.findOneAndUpdate(
+    { username: req.userData['username'] }, 
+    { 
+      $inc: { following: -1 } 
+    }, 
+   {new: true }
+   )
+   )
+     .then(()=>{
+      UsersModel.findOneAndUpdate(
+        { username: req.userData['username'] }, 
+        { $pull: { followingIds :  req.params.userMongoId} },
+        (err,response) => {
+          if (err) console.log(err);
+            UsersModel.findOneAndUpdate(
+            { _id: req.params.userMongoId }, 
+            { $pull: { followedBy : req.userData['userId'] } },
+            (err,response)=>{
+              if (err) console.log(err);
+                res.status(201).json({
+                serverStat:0,
+                message: "unfollowed"
+              });
+            })}
+        )})
+    })
+
+
+
+
+router.post('/update', isAuth,(req, res) => {
+  console.log(req.body)
+  UsersModel.findOneAndUpdate(
+    { username: req.userData['username'] }, 
+    req.body,
+    {new: true } ,
+    (err,response) => {
+      if (err) console.log(err);
+            res.status(201).json({
+            serverStat:0,
+            message: response
+          });
+        })}
+    );
 
 module.exports = router;
+
+//username: req.userData['username'],
