54c6eaa4f31f6b713c2bf5f1159df6b4f27d660b
jassimran1999 <jvirdi4@gmail.com>
2020-02-02 04:00:47 +0530

    front

--- blog/src/Components/Login/index.js
+++ blog/src/Components/Login/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{flag} from "react";
 import {
   BrowserRouter as Router,
   Switch,
@@ -13,66 +13,72 @@ class Login extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      file: "",
       imagePreviewUrl: "",
-      email: " ",
       redirectToHome: false,
-      flag:false,  
+      flag: true
     };
     
+  
+
+  this.handleClick = this.handleClick.bind(this);
   }
-  handleClick1=function(){
-    this.setState({flag:true});
+  handleClick=()=>{
+    this.setState(state=>({flag:!state.flag}));
 }
-handleClick2=function(){
-    this.setState({flag:false});
+myChangeHandler = (event) => {
+  this.setState({username: event.target.value});
+}
+myChangeHandler2 = (event) => {
+  this.setState({password: event.target.value});
 }
 
   render() {
     if (this.state.redirectToHome) {
-      return <Redirect to="/home" />;
+      //return <Redirect to="/home" />;
     }
 
-    let content = !!this.state.flag ? ( // right here conditional statement content will get one of the 2 navbar codes.
-        <div className="login-page">
+   
+    return (
+        <div>
+          <div value={this.state.flag}/>
+          {this.state.flag &&  <div className="login-page">
         <div className="form">
           <form className="register-form">
             <input type="text" placeholder="name"/>
             <input type="password" placeholder="password"/>
             <input type="text" placeholder="email address"/>
-            <button>create</button>
-            <p className="message" onClick={this.handleClick1}>Already registered? <a href="">Sign In</a></p>
+            <button >create</button>
+            <p className="message" onClick={this.handleClick}>Already registered? </p>
           </form>
           </div>
-          </div>
-      ) : (
-          <div className="login-page">
+          </div>}
+            {
+              !this.state.flag && 
+              <div className="login-page">
               <div className="form">
-        <form className="login-form">
-            <input type="text" placeholder="username"/>
-            <input type="password" placeholder="password"/>
-            <button>login</button>
-            <p className="message" onClick={this.handleClick2}>Not registered? <a href="">Create an account</a></p>
+        <form className="login-form" onSubmit={this._addUser}>
+            <input type="text" placeholder="username" value={this.state.username} onChange={this.myChangeHandler}/>
+            <input type="password" placeholder="password" value={this.state.password} onChange={this.myChangeHandler2}/>
+            <button >login</button>
+            <p className="message" onClick={this.handleClick}>Not registered? Create an account</p>
           </form>
         </div>
       </div>  
-      );
-   
-    return (
-        <div>
-            {content}
+            }
         </div>
     );
   }
 
-  _addUser = event => {
+  _addUser = (event) => {
     event.preventDefault();
-    let user = {
-      username: this.refs.username.value,
-      password: this.refs.password.value
-    };
 
-    fetch("http://ec2-54-159-137-67.compute-1.amazonaws.com:5000/users/Signin", {
+    let user = {
+      username: this.state.username,
+      password: this.state.password
+    };
+    alert(user.username)
+    console.log(this.state.username)
+    fetch("http://ec2-54-159-137-67.compute-1.amazonaws.com:5000/users/login", {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
@@ -80,10 +86,14 @@ handleClick2=function(){
       body: JSON.stringify(user)
     })
       .then(res => {
-        if (res.ok) return res.json();
+        
+        if (res.ok) 
+        {
+          return res.json();
+        }
       })
       .then(res => {
-        console.log(`User added successfully: ${res}`);
+        localStorage.setItem('token',res.token)
         this.setState({ redirectToHome: true });
       })
       .catch(err => {
