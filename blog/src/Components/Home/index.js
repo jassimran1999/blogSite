d2e8a2eda3ba6f882c190688f06292ceec1903ff
jassimran1999 <jvirdi4@gmail.com>
2020-02-02 09:56:30 +0530

    new

--- blog/src/Components/Home/index.js
+++ blog/src/Components/Home/index.js
@@ -29,7 +29,7 @@ class Home extends React.Component {
 
   homePostData = () => {
     //let id = "localhost:5000/queryParams?id:"+this.props.match.params.userId
-    fetch("http://http://ec2-54-159-137-67.compute-1.amazonaws.com:5000/home/homedata")
+    fetch("http://ec2-54-159-137-67.compute-1.amazonaws.com:5000/home/homedata")
       .then(response => {
         return response.json();
       })
